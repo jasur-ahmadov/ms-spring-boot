4311e39057b4bdc11511076bada2cfd56f4a7a9a
Jasur <jasur.ahmadov@bankrespublika.az>
2024-10-14 15:58:26 +0400

    master

--- src/main/java/com/example/MsSpringBootApplication.java
+++ src/main/java/com/example/MsSpringBootApplication.java
@@ -1,12 +1,34 @@
 package com.example;
 
+import com.example.config.PropertiesConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsSpringBootApplication {
+@Slf4j
+@RequiredArgsConstructor
+public class MsSpringBootApplication implements CommandLineRunner {
+
+    private final PropertiesConfig propertiesConfig;
+
+    @Value("${has.account}")
+    private Boolean hasAccount;
 
     public static void main(String[] args) {
         SpringApplication.run(MsSpringBootApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        if (hasAccount) {
+            log.info("hasAccount");
+            propertiesConfig.getNames().forEach(System.out::println);
+        } else {
+            log.info("Does not possess any accounts");
+        }
     }
 }
---
+++ src/main/java/com/example/config/PropertiesConfig.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "student")
+@Data
+public class PropertiesConfig {
+
+    private List<String> names;
+}
